f86bd7f3991a9bae36cc2a3d7236de8af8b6658b
Kenan7 <kenankzmv@gmail.com>
2018-10-11 21:19:27 +0300

    Adding celery tasks

---
+++ jobs/celery.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobs.settings')
+
+app = Celery('jobs')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
