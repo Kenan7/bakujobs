c15e7885c5fd4e87e4faf046cac51b16a67a51b7
Kenan7 <kenankzmv@gmail.com>
2018-10-11 21:27:45 +0300

    Added LogoutView

--- employer/views.py
+++ employer/views.py
@@ -1,9 +1,20 @@
-from django.contrib.auth import authenticate, login, get_user_model
-from django.views.generic import CreateView, FormView
-from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.views.generic import CreateView, FormView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from .forms import LoginForm, RegisterForm
 from django.utils.http import is_safe_url
-from django.http import HttpResponse
+from authtools import views as authviews
+from django.urls import reverse_lazy
+from .models import Employer
+
+class EmployerJobList(LoginRequiredMixin, ListView):
+    template_name = 'employer/job_list.html'
+    def get_queryset(self, *args, **kwargs):
+        slug = self.request.user.slug
+        qs = get_object_or_404(Employer, slug=slug)
+        return qs.job_set.all()
 
 
 class LoginView(FormView):
@@ -33,3 +44,8 @@ class RegisterView(CreateView):
     context_object_name = 'form'
     template_name = 'bakujobs/register.html'
     success_url = '/login/'
+
+
+class LogOutView(authviews.LogoutView):
+    url = reverse_lazy('home')
+
